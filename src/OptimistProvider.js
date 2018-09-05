cff3f4c7eb2e87d3edcc4e128380b1a7da35e79c
Kasper Pihl Tornøe <kasper@pihl.it>
2018-09-05 13:09:53 +0200

    master: added hasMore to next

--- src/OptimistProvider.js
+++ src/OptimistProvider.js
@@ -1,5 +1,5 @@
-import React, { PureComponent } from 'react';
-import Context from './utils/getContext';
+import React, { PureComponent } from "react";
+import Context from "./utils/getContext";
 
 export default class extends PureComponent {
   constructor(props) {
@@ -10,62 +10,70 @@ export default class extends PureComponent {
         get: this.onGet,
         unset: this.onUnset,
         _updatedKeyMap: {},
-        defaultOptions: Object.assign({
-          serial: false,
-          debounce: 0,
-          throttle: 0,
-          clearOnError: true,
-        }, props.defaultOptions),
-      },
-    }
+        defaultOptions: Object.assign(
+          {
+            serial: false,
+            debounce: 0,
+            throttle: 0,
+            clearOnError: true
+          },
+          props.defaultOptions
+        )
+      }
+    };
     this.data = {};
     this.queues = {};
     this.runningOptions = {};
   }
-  onSet = (options) => {
-    const { key, value, handler } = options;
-    this.data[key] = value;
+  onSet = options => {
+    const { key, value, handler } = options;
+    this.data[key] = value;
     this.updateStateForKey(key);
-    if(typeof handler === 'function') {
+    if (typeof handler === "function") {
       this.addToQueue(options);
     } else {
       return this.onNext.bind(this, key);
     }
-  }
-  onUnset = (key) => {
+  };
+  onUnset = key => {
     delete this.data[key];
     delete this.runningOptions[key];
     delete this.queues[key];
     this.updateStateForKey(key);
-  }
-  onGet = (key, fallback) => {
-    if(typeof this.data[key] !== 'undefined') {
+  };
+  onGet = (key, fallback) => {
+    if (typeof this.data[key] !== "undefined") {
       return this.data[key];
     }
     return fallback;
-  }
+  };
   onNext = (key, error) => {
+    if (!this.runningOptions[key]) return false;
     const options = this.runningOptions[key];
     delete this.runningOptions[key];
-    if(error) {
-      this.handleError(options, error)
-    } else if(!this.queues[key].length) {
+    if (error) {
+      this.handleError(options, error);
+    } else if (!this.queues[key].length) {
       delete this.data[key];
       this.updateStateForKey(key);
     } else {
       this.runQueue(key);
+      return true; // Has more.
     }
-    
-  }
+    return false;
+  };
   handleError(options, error) {
-    const { clearOnError } = options;
-    if(clearOnError) {
+    const { clearOnError } = options;
+    if (clearOnError) {
       this.queues[key] = [];
       delete this.data[key];
       this.updateStateForKey(key);
     } else {
       this.queues[key] = [options].concat(this.queues[key]);
-      console.log('not sure yet what to do here. (on error without clearOnError)', error);
+      console.log(
+        "not sure yet what to do here. (on error without clearOnError)",
+        error
+      );
       // What should I do in case of an error and behaviour is not to clear??..
       /*
         I'm thinking calling an error handler with a retry callback.
@@ -74,25 +82,25 @@ export default class extends PureComponent {
     }
   }
   updateStateForKey(key) {
-    this.state.value._updatedKeyMap[key] = new Date().toISOString(); 
+    this.state.value._updatedKeyMap[key] = new Date().toISOString();
     const newValue = Object.assign({}, this.state.value);
-    this.setState( { value: newValue} );
+    this.setState({ value: newValue });
   }
   addToQueue(options) {
     const { key, serial } = options;
-    if(!this.queues[key] || !serial) {
+    if (!this.queues[key] || !serial) {
       this.queues[key] = [];
     }
     this.queues[key].push(options);
     this.runQueue(key);
   }
   runQueue(key) {
-    if(this.runningOptions[key]) return;
-    
+    if (this.runningOptions[key]) return;
+
     const options = this.queues[key].shift();
     this.runningOptions[key] = options;
-    
-    if(typeof options.handler !== 'function') {
+
+    if (typeof options.handler !== "function") {
       return this.onNext(key);
     }
     options.handler(this.onNext.bind(this, key));
@@ -100,9 +108,7 @@ export default class extends PureComponent {
   render() {
     const { children } = this.props;
     return (
-      <Context.Provider value={this.state.value}>
-        {children}
-      </Context.Provider>
+      <Context.Provider value={this.state.value}>{children}</Context.Provider>
     );
   }
 }
